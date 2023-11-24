42eb6399567fdef64ab33fde26ecadb4b5c0b416
Alysson Batista <alyssonbatista241@gmail.com>
2023-11-24 11:18:53 -0300

    fiz uma parte do storeconfig

--- src/components/Intervalo.jsx
+++ src/components/Intervalo.jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import Card from './Card'
 import './Intervalo.css'
 import {connect} from 'react-redux'
-
+import { alterarNumeroMinimo } from '../actions/numeros.js'
 
 const Intervalo = (props) => {
   const {min,max} = props;
+  
+
   return (
     
       <Card title='Intervalo de números' red>
        <div className='intervalo'>
         <span>
             <strong> Mínimo: { }</strong>
-            <input type="number" value={min} readOnly />
+            <input type="number" value={min} onChange={e => props.alteraNumero(+e.target.value)} />
           </span>
           <span>
             <strong> Máximo:</strong>
@@ -31,4 +33,12 @@ const mapStateToProps = (state) => {
   }
 }
 
-export default connect(mapStateToProps)(Intervalo)
+const mapActionCreatorsToProps = (dispatch) => {
+  return{
+    alteraNumero(novoNumero){
+      const action = alterarNumeroMinimo(novoNumero)
+      dispatch(action)
+    }
+  }
+}
+export default connect(mapStateToProps,mapActionCreatorsToProps)(Intervalo)
--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,13 +1,27 @@
 import { createStore, combineReducers } from "redux";
 
 const reducers = combineReducers({
-  numeros: function(state,action){
-    return{
-      min: 7,
-      max: 31
+  numeros: function (state, action) {
+    console.log('Reducer Numeros...')
+    console.log(state, ' ', action)
+
+    switch (action.type) {
+      case 'NUM_MIN_ALTERADO':
+            return {
+              ...state,
+              min: action.payload
+            }
+      default:
+        return {
+          min: 7,
+          max: 76
+        }
     }
+  
   },
-  nomes: function(state,action){
+  nomes: function (state, action) {
+    console.log('Reducer Numeros...')
+    console.log(state, ' ', action)
     return [
       'Ana',
       'Bia',
@@ -16,7 +30,7 @@ const reducers = combineReducers({
   }
 })
 
-function storeConfig(){
+function storeConfig() {
   return createStore(reducers)
 }
 
